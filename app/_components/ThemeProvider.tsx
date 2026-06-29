bd4cc52b961545d2a44574e668981c6b6b8b054b
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2026-06-29 22:21:36 +0200

    Update

--- app/_components/ThemeProvider.tsx
+++ app/_components/ThemeProvider.tsx
@@ -1,9 +1,21 @@
+// app/_components/ThemeProvider.tsx
 "use client";
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+};
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  );
 }
