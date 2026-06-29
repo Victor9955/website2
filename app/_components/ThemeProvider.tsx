e01e443af0375422166567508b08843f1d76fb20
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2026-06-29 23:03:03 +0200

    Update ThemeProvider.tsx

--- app/_components/ThemeProvider.tsx
+++ app/_components/ThemeProvider.tsx
@@ -1,24 +1,9 @@
-// app/_components/ThemeProvider.tsx
 "use client";
 
+import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ReactNode } from "react";
+import { type ThemeProviderProps } from "next-themes/dist/types";
 
-interface ThemeProviderProps {
-  children: ReactNode;
-}
-
-export function ThemeProvider({ children }: ThemeProviderProps) {
-  return (
-    <div suppressHydrationWarning>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </NextThemesProvider>
-    </div>
-  );
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
